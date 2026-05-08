779aae11e12c4f5b54c6ca7c41bf74a84b612540
errorranjith <branjithkumar2005@gmail.com>
2026-05-08 11:02:05 +0530

    java files updated

--- src/main/java/Engineeringcutoff.java
+++ src/main/java/Engineeringcutoff.java
@@ -1,1 +1,12 @@
-
+public void main(String args[]) {
+public static void main( double math){
+return math;}
+public static void phy(double phy){
+return phy;}
+public static void che(double che){
+return che;}
+public static void cutoff(double cutoff){
+int cutoff=math+[phy+che/2];
+return cutoff;}
+string che,phy,math;
+}
--- src/test/java/EngineeringcutoffTest.java
+++ src/test/java/EngineeringcutoffTest.java
@@ -1,1 +1,17 @@
+import java.util.io.*;
+import java.util.jtest;
+public static void main(string args[]){
+@Test
+public static void math(){
+return math;}
+@Test
+public static void che(){
+return che;}
+@Test
+public static void phy(){
+return phy;}
+@Test
+public static void cutoff(){
+return cutoff;}
+}
 
